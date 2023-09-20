d47e97ab7603e045a058cdfb2421e59e7f5ab2c8
savaggge <alexandr.vagheliuc@isa.utm.md>
2023-09-20 16:14:31 +0300

    Language Basics

---
+++ Basics/lista de numere/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // a) Citește lista de numere de la tastatură și salvează-le într-un array
+        Console.WriteLine("Introduceti o lista de numere separate prin spatiu:");
+        string input = Console.ReadLine();
+        string[] numereString = input.Split(' ');
+        double[] numere = new double[numereString.Length];
+
+        for (int i = 0; i < numereString.Length; i++)
+        {
+            if (double.TryParse(numereString[i], out double numar))
+            {
+                numere[i] = numar;
+            }
+            else
+            {
+                Console.WriteLine($"Elementul {numereString[i]} nu este un numar valid si va fi ignorat.");
+            }
+        }
+
+        // b) Parcurge array-ul și afișează numerele care nu sunt întregi
+        Console.WriteLine("Numerele care nu sunt intregi sunt:");
+        foreach (double numar in numere)
+        {
+            if (!EsteIntreg(numar))
+            {
+                Console.WriteLine(numar);
+            }
+        }
+
+        // c) Găsește și afișează cel mai mic număr fără a folosi Math.Min
+        double minim = GasesteMinim(numere);
+        Console.WriteLine($"Cel mai mic numar este: {minim}");
+    }
+
+    // Verifică dacă un număr este întreg
+    static bool EsteIntreg(double numar)
+    {
+        return numar == Math.Floor(numar);
+    }
+
+    // Găsește cel mai mic număr într-un array de numere
+    static double GasesteMinim(double[] numere)
+    {
+        if (numere.Length == 0)
+        {
+            throw new ArgumentException("Array-ul de numere este gol.");
+        }
+
+        double minim = numere[0];
+        for (int i = 1; i < numere.Length; i++)
+        {
+            if (numere[i] < minim)
+            {
+                minim = numere[i];
+            }
+        }
+
+        return minim;
+    }
+}
---
+++ Basics/oglinda1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+        bool isNumberValid = false;
+
+        do
+        {
+            Console.Write("Introdu un numar cu cel putin 3 cifre: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out num) && input.Length >= 3)
+            {
+                isNumberValid = true;
+            }
+            else
+            {
+                Console.WriteLine("Numarul introdus nu are cel putin 3 cifre. Te rog sa introdu un alt numar.");
+            }
+        } while (!isNumberValid);
+
+        int reversedNum = ReverseNumber(num);
+        Console.WriteLine($"Valoarea in oglinda a numarului este: {reversedNum}");
+
+        if (IsPerfectSquare(reversedNum))
+        {
+            Console.WriteLine($"{reversedNum} este un patrat perfect.");
+        }
+        else
+        {
+            Console.WriteLine($"{reversedNum} nu este un patrat perfect.");
+        }
+    }
+
+    static int ReverseNumber(int num)
+    {
+        int reversedNum = 0;
+        while (num > 0)
+        {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+        return reversedNum;
+    }
+
+    static bool IsPerfectSquare(int num)
+    {
+        int sqrt = (int)Math.Sqrt(num);
+        return sqrt * sqrt == num;
+    }
+}
